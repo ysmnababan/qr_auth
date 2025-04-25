6c5802235bd0536fc8c16ae09c1f82d497a4e468
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-04-25 15:19:14 +0700

    add wg for mockpush

--- pusherutil/mocks/pusher_mock.go
+++ pusherutil/mocks/pusher_mock.go
@@ -1,6 +1,9 @@
 package mocks
 
+import "sync"
+
 type MockPush struct {
+	Wg           *sync.WaitGroup
 	TriggerCalls []TriggerCall
 }
 
@@ -15,6 +18,9 @@ func NewMockPush() *MockPush {
 }
 
 func (m *MockPush) Trigger(channel string, eventName string, data any) error {
+	if m.Wg != nil {
+		defer m.Wg.Done()
+	}
 	m.TriggerCalls = append(m.TriggerCalls, TriggerCall{
 		Channel:   channel,
 		EventName: eventName,
